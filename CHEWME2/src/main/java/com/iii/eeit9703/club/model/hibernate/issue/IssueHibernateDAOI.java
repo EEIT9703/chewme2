1f4e83cec32299eae8f596c74d51b64adf78ba74
陳泓宇 <klyvechenPG@gmail.com>
2017-10-23 20:04:47 +0800

    完成Hibernate IssueDAO

--- CHEWME2/src/main/java/com/iii/eeit9703/club/model/hibernate/issue/IssueHibernateDAOI.java
+++ CHEWME2/src/main/java/com/iii/eeit9703/club/model/hibernate/issue/IssueHibernateDAOI.java
@@ -8,8 +8,8 @@ import com.iii.eeit9703.club.model.IssueVO;
 public interface IssueHibernateDAOI {
 	public List<IssueVO> getAll();
 	public void getOne(Integer issueId);
-	public void insert(ClubVO IssueVO);
-	public void update(ClubVO IssueVO);
+	public void insert(IssueVO IssueVO);
+	public void update(IssueVO IssueVO);
 	public void delete(Integer issueId);
 	
 }
