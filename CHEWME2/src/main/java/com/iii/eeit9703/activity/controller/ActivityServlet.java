525b8ff9f495b6ef52ea4f1fe00369eca251f790
廖維浪 <william82628@outlook.com>
2017-10-25 16:34:39 +0800

    test...

--- CHEWME2/src/main/java/com/iii/eeit9703/activity/controller/ActivityServlet.java
+++ CHEWME2/src/main/java/com/iii/eeit9703/activity/controller/ActivityServlet.java
@@ -43,10 +43,11 @@ public class ActivityServlet extends HttpServlet {
 		resp.setContentType("text/html; charset=UTF-8");
 		
 		
-		
 		String action = req.getParameter("action");
 		PrintWriter out = resp.getWriter();
 
+		System.out.println(action);
+		
 		//新增活動
 /*		if("insert".equals(action)){  //來自XXX.jsp的請求
 				
@@ -115,52 +116,56 @@ public class ActivityServlet extends HttpServlet {
 		
 		
 		//活動上架
-		if("update".equals(action)){  //來自/createActivity.jsp 請求
+		if("Updata".equals(action)){  //來自/createActivity.jsp 請求
 			
+			System.out.println("XX");
 			List<String> errorMsgs = new LinkedList<String>();
 			req.setAttribute("errorMsgs", errorMsgs);
 			
 			try {
 				//1.接收請求
-				Integer actID = new Integer(req.getParameter("actID").trim());			
-				String act_name = req.getParameter("act_name");
-				Integer act_groups = new Integer(req.getParameter("act_groups"));
-				Integer act_current = new Integer(req.getParameter("act_groups"));
-				Date BDate = java.sql.Date.valueOf(req.getParameter("BDate"));
-				Date EDate = java.sql.Date.valueOf(req.getParameter("EDate"));
-				Integer activity_state = new Integer(req.getParameter("activity_state"));
+				Integer actID = new Integer(req.getParameter("a"));
+				System.out.println(actID);
+				String act_name = req.getParameter("b");
+				Integer act_groups = new Integer(req.getParameter("c"));
+				Date BDate = java.sql.Date.valueOf(req.getParameter("d"));
+				Date EDate = java.sql.Date.valueOf(req.getParameter("e"));
+				Integer activity_state = new Integer(req.getParameter("f"));
+				
+				System.out.println("HELLO");
 				
 //			Integer actID =new Integer(req.getParameter("actID").trim());
 				
-				ActivityVO activityVO = new ActivityVO();
-				
-				activityVO.setAct_name(act_name);
-				activityVO.setAct_groups(act_groups);
-				activityVO.setAct_current(act_current);
-				activityVO.setBDate(BDate);
-				activityVO.setEDate(EDate);
-				activityVO.setActivity_state(activity_state);
-				
-				if(!errorMsgs.isEmpty()){
-					req.setAttribute("activityVO", activityVO); //含有輸入錯誤的activityVO 也存入req
-					RequestDispatcher failureView =req.getRequestDispatcher("/act/createAct.jsp");
-					failureView.forward(req, resp);
-					return;
-				}
-				
-				//2.開始修改資料
-				ActService actSvc = new ActService();
-				activityVO = actSvc.updateAct(actID, act_name, act_groups, act_current, BDate, EDate, activity_state);
-				
-				//修改完成  準備轉交
-				req.setAttribute("activityVO", activityVO);  //資料庫update成功後 正確的activityVO 存入req
-				String url = "/act/createActi.jsp";
-				RequestDispatcher successView = req.getRequestDispatcher(url);
-				successView.forward(req, resp);
+//				ActivityVO activityVO = new ActivityVO();
+//				
+//				activityVO.setActID(actID);
+//				activityVO.setAct_name(act_name);
+//				activityVO.setAct_groups(act_groups);
+//			
+//				activityVO.setBDate(BDate);
+//				activityVO.setEDate(EDate);
+//				activityVO.setActivity_state(activity_state);
+//				
+//				if(!errorMsgs.isEmpty()){
+//					req.setAttribute("activityVO", activityVO); //含有輸入錯誤的activityVO 也存入req
+//					RequestDispatcher failureView =req.getRequestDispatcher("/act/createAct.jsp");
+//					failureView.forward(req, resp);
+//					return;
+//				}
+//				
+//				//2.開始修改資料
+//				ActService actSvc = new ActService();
+//				actSvc.updateAct(act_name, act_groups, BDate, EDate, activity_state);
+//				
+//				//修改完成  準備轉交
+//				req.setAttribute("activityVO", activityVO);  //資料庫update成功後 正確的activityVO 存入req
+//				String url = "/act/createAct.jsp";
+//				RequestDispatcher successView = req.getRequestDispatcher(url);
+//				successView.forward(req, resp);
 				
 			} catch (Exception e) {
 				errorMsgs.add("修改資料失敗"+e.getMessage());
-				RequestDispatcher failureView =req.getRequestDispatcher("/act/createActi.jsp");
+				RequestDispatcher failureView =req.getRequestDispatcher("/act/createAct.jsp");
 				failureView.forward(req, resp);
 				e.printStackTrace();
 			}
