bc1680f1e56fb85cc6f06fa9a4d8a0685ce0ed11
黃彥坤 <apple@gmail>
2017-10-25 14:51:18 +0800

    活動 會員 加入 hibernate
    collection package產生

---
+++ CHEWME2/src/main/java/com/iii/eeit9703/collection/CollectionVO.java
@@ -0,0 +1,45 @@
+package com.iii.eeit9703.collection;
+
+import java.io.Serializable;
+
+import com.iii.eeit9703.activity.model.ActivityVO;
+import com.iii.eeit9703.member.model.MemVO;
+
+
+public class CollectionVO implements Serializable {
+	private ActivityVO activityVO;
+	private MemVO MemVO;
+	private String context;
+	private Integer status;
+	
+	
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
+	
+	public String getContext() {
+		return context;
+	}
+	public void setContext(String context) {
+		this.context = context;
+	}
+	public ActivityVO getActivityVO() {
+		return activityVO;
+	}
+	public void setActivityVO(ActivityVO activityVO) {
+		this.activityVO = activityVO;
+	}
+	public MemVO getMemVO() {
+		return MemVO;
+	}
+	public void setMemVO(MemVO memVO) {
+		MemVO = memVO;
+	}
+	
+
+	
+}
