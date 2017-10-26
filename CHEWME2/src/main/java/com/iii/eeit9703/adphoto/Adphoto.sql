95453f8a622c6bf6855cbfff02e20ae74e6c1735
陳泓宇 <klyvechenPG@gmail.com>
2017-10-26 18:27:46 +0800

    DATABASE sql 指令修改

---
+++ CHEWME2/src/main/java/com/iii/eeit9703/adphoto/Adphoto.sql
@@ -0,0 +1,21 @@
+
+--�ϥ�CMDB��Ʈw
+use CMDB;
+go
+
+--�إ߽s���{���
+
+IF EXISTS (SELECT name FROM sysobjects
+            WHERE type='u' AND name = 'adphoto')      
+BEGIN
+drop table adphoto;
+
+END                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+go
+
+
+create table adphoto(
+	photo_no	int identity(1,1) PRIMARY KEY,
+	name		varchar(30)		, 
+	photo		varchar(MAX)	NOT NULL
+)
