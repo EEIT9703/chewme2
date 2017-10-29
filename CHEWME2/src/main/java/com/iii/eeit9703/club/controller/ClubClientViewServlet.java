d8ad1cd8e3debb2cad1ec82fd6411195d630b279
陳泓宇 <klyvechenPG@gmail.com>
2017-10-29 17:24:25 +0800

    client view 上傳

--- CHEWME2/src/main/java/com/iii/eeit9703/club/controller/ClubClientViewServlet.java
+++ CHEWME2/src/main/java/com/iii/eeit9703/club/controller/ClubClientViewServlet.java
@@ -2,7 +2,12 @@ package com.iii.eeit9703.club.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -26,14 +31,57 @@ import com.iii.eeit9703.club.model.IssueVO;
 @WebServlet("/club/clubClientView.do")
 public class ClubClientViewServlet extends HttpServlet {
 	
+	
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doService(request,response);
+		try {
+			doService(request,response);
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doService(request,response);
+		try {
+			doService(request,response);
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	protected void doService(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	protected void doService(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SecurityException, ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
 		System.out.println("Start the ClubClientVIEW");
+		
+		
+		response.setCharacterEncoding("UTF-8");
+		response.setHeader("content-type", "text/html;charset=UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		
 		request.setCharacterEncoding("UTF-8");		
 		String action = request.getParameter("action");
 		System.out.println("The action is " + action);
@@ -56,38 +104,60 @@ public class ClubClientViewServlet extends HttpServlet {
 		/*服務從點選導過來的,顯示club的Service*/
 		if(action.matches("loadIssues")){		
 			System.out.println("In ClubClientVIEW, get in to load issues.");
+			/*準備get方法內傳回參數的基本物件(DAO及writer)*/
 			PrintWriter out = response.getWriter();
 			IssueService is = new IssueService();
-			List<IssueVO> issueVO_list = is.getIssueList();
-			Map issueMap = new HashMap();
-			Map commentMap = new HashMap();
-			List<Map> issueList_json;
-			Set<Map> commentList_json; 
+			List<IssueVO> issueVO_list = is.getIssueList();						
+			List<Map> issueList_json = new ArrayList();			
 			IssueVO isvo;
-			Set<CommentVO> cmvoList;
-			for(int i =0;i<issueVO_list.size();i++){
-				isvo = issueVO_list.get(i);
+			List<Map> commentList_json;
+			Map issueMap;
+			CommentVO cmvo;
+			Set<CommentVO> cmvoSet;
+			Map commentMap;
+			for(Iterator<IssueVO> itero=issueVO_list.iterator();itero.hasNext();){
+				issueMap = new HashMap();
+				commentList_json  = new ArrayList(); 
+				isvo = itero.next();
 				issueMap.put("issueId", isvo.getIssueId());
 				issueMap.put("clubId", isvo.getClubId());
-				issueMap.put("issueTitle",isvo.getIssueTitle());
+				issueMap.put("issueTitle",   URLDecoder.decode(isvo.getIssueTitle()));
 				issueMap.put("issueContent", isvo.getIssueContent());
 				issueMap.put("proposerId", isvo.getProposerId());
 				issueMap.put("issuepic", isvo.getIssuepic());
-				cmvoList=isvo.getComments();
-				for(int i =0;;){
-					commentMap.put("", value)
-					commentList_json=add()
+				System.out.println("issue map ok");
+				cmvoSet=isvo.getComments();
+				System.out.println("the cmvoSet's size is "+cmvoSet.size());
+				for(Iterator<CommentVO> iter=cmvoSet.iterator();iter.hasNext();){
+					commentMap = new HashMap();
+					cmvo = iter.next();
+					System.out.println(cmvo.getCommentId());
+					for(Method method: Class.forName("com.iii.eeit9703.club.model.CommentVO").getDeclaredMethods()){
+						System.out.println(method.getName());
+						if(method.getName().matches("get(.*)")){
+								if(method.getReturnType().equals(Integer.class)){									
+									commentMap.put(method.getName(),method.invoke(cmvo,null));
+								}else{
+									commentMap.put(method.getName(),
+											   URLDecoder.decode(method.invoke(cmvo,null).toString(),"utf-8"));
+								}
+								System.out.println(method.getName());							
+						}
+						
+					}
+					commentList_json.add(commentMap);
+					
 				}
+				
+				issueMap.put("comments",commentList_json);
+				System.out.println("issue map ok");
 				issueList_json.add(issueMap);
-				//StringHelper.testEncode(isvo.getIssueTitle());
 				//StringHelper.testEncode(isvo.getIssueContent());
 			}
-			   
-			
-			
 			String jsonString = JSONValue.toJSONString(issueList_json);
 			System.out.println(jsonString);
 			out.println(jsonString);
+			//return;
 		}
 		
 	}
