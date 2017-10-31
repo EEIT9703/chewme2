2fc2788e07f75f0c04cef3bd76c2726600d02f4f
陳泓宇 <klyvechenPG@gmail.com>
2017-10-31 13:49:44 +0800

    01:48 暫時push 內容涵蓋 bridge內的資料

--- CHEWME2/src/main/java/com/iii/eeit9703/member/model/MemberSession.java
+++ CHEWME2/src/main/java/com/iii/eeit9703/member/model/MemberSession.java
@@ -3,23 +3,53 @@ package com.iii.eeit9703.member.model;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.iii.eeit9703.club.model.ClubService;
+
 public class MemberSession {
 	private Integer memId;
+	private List<Integer> joinedClubList;
+	private List<Integer> ownClubList;
+	private List<Integer> ownActivityList;
+	//private List<> 
+	
+	private MemVO memVO;
+	
+	
+	MemberSession(){
+		joinedClubList = new LinkedList<Integer>();
+	}
+	MemberSession(MemVO memVO){
+		this();
+		setMemId(this.memVO.getMemId());
+		setMemVO(memVO);
+		setAuthorizedclubList();
+			
+		
+	
+	}
+	public void setMemVO(MemVO memVO){
+		this.memVO = memVO;
+	}
+	public MemVO getMemVO(){
+		return this.memVO;
+	}
+	
 	public Integer getMemId() {
 		return memId;
 	}
 	public void setMemId(Integer memId) {
 		this.memId = memId;
 	}
-	List<Integer> authorizedClubList;
-	MemberSession(){
-	authorizedClubList = new LinkedList();
+	public void setAuthorizedclubList(){
+		ClubService cs = new ClubService();
+		
 	}
-	void addAuthorizedClubList(Integer clubId){
-		authorizedClubList.add(clubId);
+	
+	public void addjoinedClubList(Integer clubId){
+		joinedClubList.add(clubId);
 	}
-	boolean checkAuthorized(Integer clubId){
-		if(authorizedClubList.contains(clubId)){
+	public boolean checkjoined(Integer clubId){
+		if(joinedClubList.contains(clubId)){
 			return true;
 		} else{
 			return false;			
