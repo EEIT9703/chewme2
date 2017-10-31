4287d10eb152ca781a732a6e4d009459cb69d9ad
陳泓宇 <klyvechenPG@gmail.com>
2017-10-31 21:31:00 +0800

    create clubphotodao的資料查詢

---
+++ CHEWME2/src/main/java/com/iii/eeit9703/club/6_CreateTableClubPhoto.sql
@@ -0,0 +1,21 @@
+
+use CMDB;
+go
+
+
+IF EXISTS (SELECT name FROM sysobjects
+            WHERE type='u' AND name = 'club_photo')      
+BEGIN
+drop table club_photo;
+
+END                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+go
+
+
+create table club_photo(
+	photoId  	int identity(1,1) PRIMARY KEY,
+	clubId      int CONSTRAINT FK_PicC_ClubID FOREIGN KEY(clubID) REFERENCES clubs(clubID) on delete cascade;
+	name		varchar(30)		, 
+	photo		varchar(MAX)	NOT NULL
+	
+)
