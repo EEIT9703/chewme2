d08ee0a25c1034410d773263cb863368e24131a5
E.T <yiting.metis@gmail.com>
2017-11-01 10:15:48 +0800

    JNDI修改

--- CHEWME2/src/main/java/com/iii/eeit9703/actEditor/controller/ScheduleServlet.java
+++ CHEWME2/src/main/java/com/iii/eeit9703/actEditor/controller/ScheduleServlet.java
@@ -1,16 +1,25 @@
 package com.iii.eeit9703.actEditor.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -46,12 +55,21 @@ public class ScheduleServlet extends HttpServlet {
 
 	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException {
 		
+		Connection conn = null;
+		OutputStream os = null;
+		InputStream is = null;
+		
 		try {
 			String mission = request.getParameter("mission");
 			request.setCharacterEncoding("UTF-8");
 			String countryID = null;
 			Integer countyID = null;
 			String actIDs = null;
+			
+			Context context = new InitialContext();
+			DataSource ds = (DataSource) context.lookup("java:comp/env/jdbc/TestDB");
+			conn = ds.getConnection();
+			PreparedStatement pstmt = null;
 					
 			response.setCharacterEncoding("UTF-8");
 			PrintWriter out = response.getWriter();
@@ -95,6 +113,12 @@ public class ScheduleServlet extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
---
+++ CHEWME2/src/main/java/com/iii/eeit9703/actEditor/model/CountyDAO_JNDI.java
@@ -0,0 +1,93 @@
+package com.iii.eeit9703.actEditor.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class CountyDAO_JNDI {
+
+	private static DataSource ds = null;
+	static {
+		try {
+			Context ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/TestDB");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static final String COUNTY = "SELECT * FROM countys where countryID = ? order by countyID";
+		
+	Connection con = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	
+	
+	public ArrayList<CountyVO> getCounty(String countryID){
+
+		ArrayList<CountyVO> list = new ArrayList<CountyVO>();
+		CountyVO countyVO = null;
+
+		try {
+
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(COUNTY);
+			
+			pstmt.setString(1, countryID);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()){
+				countyVO = new CountyVO();
+				countyVO.setCountyID(rs.getInt("countyID"));
+				countyVO.setCountyName(rs.getString("countyName"));
+				countyVO.setCountryName(rs.getString("countryName"));
+				countyVO.setCountryID(rs.getString("countryID"));
+				list.add(countyVO);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			if(rs != null){
+				try {rs.close();} 
+				catch (SQLException e) {e.printStackTrace();}
+			}
+			if(pstmt != null){
+				try {pstmt.close();} 
+				catch (SQLException e) {e.printStackTrace();}
+			}
+			if(con != null){
+				try {con.close();} 
+				catch (SQLException e) {e.printStackTrace();}
+			}
+		}
+		return list;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		CountyDAO_JNDI aDao = new CountyDAO_JNDI();
+		
+		//查台北市區域
+		List<CountyVO> list = aDao.getCounty("TPE");
+		for(CountyVO area : list){
+			System.out.print(area.getCountyID()+",");
+			System.out.print(area.getCountyName()+",");
+			System.out.print(area.getCountryName()+",");
+			System.out.println(area.getCountryID());
+		}
+
+	}
+
+}
