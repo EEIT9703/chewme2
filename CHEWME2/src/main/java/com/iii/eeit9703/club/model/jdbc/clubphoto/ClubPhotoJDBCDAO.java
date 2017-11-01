20aa3970b493d878eb4ad8a7b3dd6ed457faae56
陳泓宇 <klyvechenPG@gmail.com>
2017-11-02 00:50:10 +0800

    clubphotodao commit

--- CHEWME2/src/main/java/com/iii/eeit9703/club/model/jdbc/clubphoto/ClubPhotoJDBCDAO.java
+++ CHEWME2/src/main/java/com/iii/eeit9703/club/model/jdbc/clubphoto/ClubPhotoJDBCDAO.java
@@ -2,6 +2,7 @@ package com.iii.eeit9703.club.model.jdbc.clubphoto;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,6 +11,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -34,7 +36,7 @@ public class ClubPhotoJDBCDAO implements ClubPhotoDAOI {
 			String url = "jdbc:sqlserver://localhost:1433;DatabaseName=CMDB";
 			String userid = "sa";
 			String passwd = "P@ssw0rd";
-			
+
 			try {
 				Class.forName(driver);
 				con = DriverManager.getConnection(url, userid, passwd);
@@ -50,7 +52,7 @@ public class ClubPhotoJDBCDAO implements ClubPhotoDAOI {
 	}
 
 	private static final String GET_ALL = "select photoId,clubId,name,photo from club_photo";
-	private static final String GET_ONE = "select clubId, name, photo from club_photo where photoId=?";
+	private static final String GET_PHOTO_BY_PHOTOID = "select clubId, name, photo from club_photo where photoId=?";
 	private static final String GET_PHOTOLIST_BY_CLUBID = "select photo from club_photo where clubId=?";
 	private static final String INSERT = "insert into club_photo (clubId, name, photo) values (?,?,?)";
 	private static final String UPDATE = "update club_photo set clubId=?, name=?, photo=? where photoID=?";
@@ -166,24 +168,45 @@ public class ClubPhotoJDBCDAO implements ClubPhotoDAOI {
 	public ClubPhotoVO findByPrimaryKey(Integer photoId) {
 
 		ClubPhotoVO clubPicVO = null;
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		// Connection con = null;
+		// PreparedStatement pstmt = null;
+		// ResultSet rs = null;
 
 		try {
-			//con = ds.getConnection();
-			pstmt = con.prepareStatement(GET_PHOTOLIST_BY_CLUBID);
+			// con = ds.getConnection();
+			pstmt = con.prepareStatement(GET_PHOTO_BY_PHOTOID);
 
 			pstmt.setInt(1, photoId);
 			rs = pstmt.executeQuery();
 
+			System.out.print("in the findbyphotoid");
 			while (rs.next()) {
 				clubPicVO = new ClubPhotoVO();
 				clubPicVO.setClubId(rs.getInt("clubId"));
 				clubPicVO.setName(rs.getString("name"));
-				clubPicVO.setPhotoId(rs.getInt("photoId"));
+				InputStream is = rs.getBinaryStream("photo");
 
+				int len = 100;
+				byte[] buffer = new byte[len];
+				int count;
+				try {
+					while ((count = is.read(buffer)) !=-1 ) {
+
+							System.out.println("in the while");
+							// fos2.write(buffer, 0, len);
+							System.out.println(new String(buffer, "UTf-8"));
+					}
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("--------------------------");
 			}
+			System.out.print("out of the findbyphotoid");
+
 			rs.close();
 			pstmt.close();
 			con.close();
@@ -200,20 +223,20 @@ public class ClubPhotoJDBCDAO implements ClubPhotoDAOI {
 		List<ClubPhotoVO> list = new ArrayList<ClubPhotoVO>();
 		ClubPhotoVO clubPicVO = null;
 
-		//Connection con = null;
-		//PreparedStatement pstmt = null;
-		//ResultSet rs = null;
+		// Connection con = null;
+		// PreparedStatement pstmt = null;
+		// ResultSet rs = null;
 		try {
-			//con = ds.getConnection();
-			
+			// con = ds.getConnection();
+
 			pstmt = con.prepareStatement(GET_PHOTOLIST_BY_CLUBID);
 			pstmt.setInt(1, clubId);
 			rs = pstmt.executeQuery();
 			while (rs.next()) {
 				clubPicVO = new ClubPhotoVO();
 				clubPicVO.setPhoto(rs.getBinaryStream("photo"));
-				//clubPicVO.setName(rs.getString("name"));
-				//clubPicVO.setPhotoId(rs.getInt("photoId"));
+				// clubPicVO.setName(rs.getString("name"));
+				// clubPicVO.setPhotoId(rs.getInt("photoId"));
 			}
 			list.add(clubPicVO);
 			rs.close();
@@ -222,36 +245,18 @@ public class ClubPhotoJDBCDAO implements ClubPhotoDAOI {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+
 		return list;
 	}
 
 	public static void main(String[] args) {
 
 		ClubPhotoJDBCDAO chd = new ClubPhotoJDBCDAO();
-		List<ClubPhotoVO> list = chd.getClubPhotoByClubId(7);
-		Iterator<ClubPhotoVO> iter = list.iterator();
-		for (; iter.hasNext();) {
-			System.out.println( );
-			InputStream is = iter.next().getPhoto();
-			int len = 100;
-			byte[] buffer = new byte[len];
-			int readNum = 0;
-			while (readNum >= 0) {
-				try {
-					readNum = is.read(buffer, 0, len);
-					// fos2.write(buffer, 0, len);
+		//List<ClubPhotoVO> list = chd.getClubPhotoByClubId(4);
+		ClubPhotoVO photo = chd.findByPrimaryKey(1);
 
-					System.out.print(readNum + " : ");
-					System.out.println(new String(buffer, "UTf-8"));
-					
-
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			System.out.println("--------------------------");
-		}
+	
+		
 	}
 
 }
