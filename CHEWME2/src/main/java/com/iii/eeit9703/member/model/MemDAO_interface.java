22e19990203fee3c91c6915d5069abbc2b843613
陳建業 <jianyeh@EEIT>
2017-11-01 10:29:49 +0800

    merge Mem_interface

--- CHEWME2/src/main/java/com/iii/eeit9703/member/model/MemDAO_interface.java
+++ CHEWME2/src/main/java/com/iii/eeit9703/member/model/MemDAO_interface.java
@@ -13,12 +13,9 @@ public interface MemDAO_interface {
     public void delete(Integer memId);
     public MemVO findByPrimaryKey(Integer memId);
     public List<MemVO> getAll();
-<<<<<<< HEAD
-    public Set<CollectionVO> getCollByMemNo(Integer memId);
-    public Set<ReportVO> getRepByMemNo(Integer memId);
 	public void insertGoogle(MemVO memVO);
-=======
+
     //public Set<CollectionVO> getCollByMemNo(Integer memId);
     //public Set<ReportVO> getRepByMemNo(Integer memId);
->>>>>>> branch 'master' of https://github.com/EEIT9703/chewme2.git
+
 }
