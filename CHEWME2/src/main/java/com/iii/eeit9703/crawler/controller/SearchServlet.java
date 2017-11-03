1778d696472300ff9d98b5fd082bfe43cdf41b22
莊君威 <chunwei@eeit>
2017-11-03 15:14:54 +0800

    增加搜尋功能

--- CHEWME2/src/main/java/com/iii/eeit9703/crawler/controller/SearchServlet.java
+++ CHEWME2/src/main/java/com/iii/eeit9703/crawler/controller/SearchServlet.java
@@ -1,6 +1,8 @@
 package com.iii.eeit9703.crawler.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.iii.eeit9703.crawler.model.AttrVO;
 import com.iii.eeit9703.crawler.model.SearchHibernateDAO;
 
-@WebServlet("/SearchServlet")
+@WebServlet("/attractions/SearchServlet")
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -22,15 +24,29 @@ public class SearchServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		String action = request.getParameter("action");
 		
-		if("getonesearch".equals(action)){
-			try{
-			String name = request.getParameter("searchone");			
-			SearchHibernateDAO shDAO = new SearchHibernateDAO();
-			AttrVO attrVO = shDAO.findByPK(name);
+		if("search".equals(action)){
+			Map<String, String> error = new HashMap<String, String>();
+			request.setAttribute("error", error);
 			
+			try{
+			String name = request.getParameter("tags");	
+			if (name == null || name.trim().length() == 0) {
+				error.put("nameerror", "姓名欄位不得空白");
+			}
+			System.out.println(name);
+			
+			SearchHibernateDAO shDAO = new SearchHibernateDAO();
+			AttrVO attrVO = shDAO.findByPK(name);			
 			request.setAttribute("attrVO", attrVO);
+			
+			if(!error.isEmpty()){
+				RequestDispatcher failure = request.getRequestDispatcher("chosen.jsp");
+				failure.forward(request, response);
+				return;
+			}
 			RequestDispatcher view = request.getRequestDispatcher("searchOne.jsp");
 			view.forward(request, response);
+			
 			}catch(Exception e){
 				e.printStackTrace();
 			}
