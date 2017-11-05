cd79b8d4aeb1e74d547fad0ca6743115a6598b9a
陳泓宇 <klyvechenPG@gmail.com>
2017-11-05 19:57:11 +0800

    commit

--- CHEWME2/src/main/java/com/iii/eeit9703/utility/GetImageServlet2.java
+++ CHEWME2/src/main/java/com/iii/eeit9703/utility/GetImageServlet2.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 // 從資料庫取圖放入JSP
-@WebServlet("/getImageInChewme.do")
+@WebServlet("/getImageInChewme2.do")
 public class GetImageServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
