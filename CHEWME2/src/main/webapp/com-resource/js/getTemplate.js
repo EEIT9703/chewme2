73a26ad40cbeefe4e18afb0cab7418b2df838dcf
陳泓宇 <klyvechenPG@gmail.com>
2017-11-06 20:29:58 +0800

    版型調整

--- CHEWME2/src/main/webapp/com-resource/js/getTemplate.js
+++ CHEWME2/src/main/webapp/com-resource/js/getTemplate.js
@@ -2,19 +2,46 @@
  * 
  */
 var template;
-$(document).ready(function(){
-	getTemp();
-
-})
-
+var template2;
+var template3;
+var template4;
+var template5;
 function getTemp(){
 	$.get("testFile.jsp",{},function(data){
 		console.log("in the get function");
 		template = data;
-		console.log(data);
-		console.log(template);
-		$('#test').prepend(template);
 	})
-
-	
+		$.get("testFile.jsp",{},function(data){
+		console.log("in the get function");
+		template = data;
+	})
+		$.get("testFile.jsp",{},function(data){
+		console.log("in the get function");
+		template = data;
+	})
 }
+
+
+
+$(document).ready(function(){
+	$.when(getTemp()).then(startToBuildTemplate);
+
+	console.log(template);
+
+})
+function startToBuildTemplate(){
+	 createTemplate();
+	 createTemplate();
+	 createTemplate();
+	$('#test').prepend(template);
+	$('#test').prepend(template);
+	$('#test').prepend(template);
+	$('#test').prepend(template);
+	$('#test').prepend(template);
+}
+
+function createTemplate(){
+	console.log("in the createTemplate");
+	$('#test').prepend(template);
+}
+
