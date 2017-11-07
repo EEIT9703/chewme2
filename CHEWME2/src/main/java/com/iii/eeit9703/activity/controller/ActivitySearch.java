92875cb2c602d1b56083dbed1a73ccfd2bc42cd2
廖維浪 <william82628@outlook.com>
2017-11-07 14:15:27 +0800

    UPDATA

--- CHEWME2/src/main/java/com/iii/eeit9703/activity/controller/ActivitySearch.java
+++ CHEWME2/src/main/java/com/iii/eeit9703/activity/controller/ActivitySearch.java
@@ -1,6 +1,9 @@
 package com.iii.eeit9703.activity.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -8,8 +11,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
 
-@WebServlet("/ActivitySearch")
+
+import com.iii.eeit9703.activity.model.ActService;
+import com.iii.eeit9703.activity.model.ActivityVO;
+
+
+@WebServlet("/act/ActivitySearch")
 
 @MultipartConfig(
 location="",
@@ -20,28 +29,44 @@ maxFileSize=1024*1024*1024
 public class ActivitySearch extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ActivitySearch() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		doPost(req, resp);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setHeader("content-type", "text/html;charset=UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		
+		
+		String action = req.getParameter("action");
+		PrintWriter out = resp.getWriter();
+		
+		
+		if("Search".equals(action)){
+			String act_name = req.getParameter("Search");
+			
+			System.out.println(act_name);
+			
+			ActService actSvc = new ActService();
+			ArrayList<ActivityVO> activityVO = actSvc.Search(act_name);
+			
+			JSONArray actJSON = new JSONArray(activityVO);
+			out.print(actJSON.toString());
+			System.out.println(actJSON);
+		
+			
+			}
 	}
+	
+	
+	
 
 }
--- CHEWME2/src/main/webapp/act/js/search.js
+++ CHEWME2/src/main/webapp/act/js/search.js
@@ -17,15 +17,17 @@ $(document).ready(function() {
 				
 			})
 		}
-		$.getJSON('/CHEWME2/act/actServlet?action=Search', {'search' : search}, function(data) {
+		$.getJSON('/CHEWME2/act/ActivitySearch?action=Search', {'search' : search}, function(data) {
 			console.log(data);
 			$.each(data, function(i, data) {
-				//getTemp();
+				
+				getTemp();
+				$('#div1').append(template);
 				var cell1 = $('#act_name').text(data.act_name);
 				var cell2 = $('#act_groups').text(data.act_groups);
 				var cell3 = $('#act_photo').html(data.act_photo);
 				var row = $('#div1').append([ cell1, cell2 ,cell3 ]);
-				template.prepend(row);
+				$('#div1').prepend(row);
 				
 			})
 
