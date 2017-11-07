c3bba017ee9911c1b25588e51e1c19b1e4605866
陳泓宇 <klyvechenPG@gmail.com>
2017-11-07 09:13:38 +0800

    更改版型方法

--- CHEWME2/src/main/webapp/com-resource/js/getTemplate.js
+++ CHEWME2/src/main/webapp/com-resource/js/getTemplate.js
@@ -1,47 +1,36 @@
 /**
  * 
  */
-var template;
+var template1;
 var template2;
 var template3;
 var template4;
 var template5;
-function getTemp(){
-	$.get("testFile.jsp",{},function(data){
-		console.log("in the get function");
-		template = data;
+
+
+$(document).ready(function() {
+	var fun1 = $.get("testFile.jsp", {})
+	var fun2 = $.get("_commentTemplate.jsp", {})
+	var fun3 = $.get("_issueTemplate.jsp", {})
+
+	$.when(fun1,fun2,fun3).then( function(data1,data2,data3) {
+		template1 = data1;
+		template2 = data2;
+		template3 = data3;
+		startToBuildTemplate();
 	})
-		$.get("testFile.jsp",{},function(data){
-		console.log("in the get function");
-		template = data;
-	})
-		$.get("testFile.jsp",{},function(data){
-		console.log("in the get function");
-		template = data;
-	})
-}
-
-
-
-$(document).ready(function(){
-	$.when(getTemp()).then(startToBuildTemplate);
-
-	console.log(template);
 
 })
-function startToBuildTemplate(){
-	 createTemplate();
-	 createTemplate();
-	 createTemplate();
-	$('#test').prepend(template);
-	$('#test').prepend(template);
-	$('#test').prepend(template);
-	$('#test').prepend(template);
-	$('#test').prepend(template);
+function startToBuildTemplate() {
+	console.log("start to build tmeplate");
+	console.log(template1);
+	console.log(template2);
+	console.log(template3);
+	createTemplate();
+	
 }
 
-function createTemplate(){
+function createTemplate() {
 	console.log("in the createTemplate");
-	$('#test').prepend(template);
+	$('#test').append(template1).append(template2).append(template3);
 }
-
